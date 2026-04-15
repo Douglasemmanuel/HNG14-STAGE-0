9aa809b546769d9c6754648047fe62c5deb7a4ff
DouglasEmmanuel <emmanueldouglas2121@gmail.com>
2026-04-14 19:03:50 -0700

    Add AJAX form handling and syntax highlighting scripts
    - Introduced `load-ajax-form.js` to enable AJAX form submission on document ready.
    - Added `prettify-min.js` for enhanced syntax highlighting of code blocks in the application.

--- hng_stage0/settings.py
+++ hng_stage0/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,25 +25,27 @@ SECRET_KEY = 'django-insecure-d232x&^r--frr+*lwzong%ly*_qh8oca&4=#iyxxa-&gwtzt6h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
     "corsheaders",
-     "rest_framework",
+    "rest_framework",
+    'core',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.whiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
       "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
@@ -120,5 +122,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core/static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CORS_ALLOW_ALL_ORIGINS = True
